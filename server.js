b90b32069e59173fa0369dee51e81f50e28431c1
Mohit <mohitshah7777@gmail.com>
2021-06-28 11:46:15 +0530

    feat: implemented logger for info and error

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('./config/database.config');
 require('dotenv')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./app/swagger/swagger.json');
+const logger = require("./logger/logger");
 
 // Create express app
 const app = express();
@@ -32,7 +33,7 @@ require('./app/routes/routes')(app);
 
 // listen for requests
 app.listen(port, () => {
-    console.log("Server is listening on port", port);
+    logger.log(`info`,`Server is listening on port : ${port}`);
 });
 
 module.exports = app;
