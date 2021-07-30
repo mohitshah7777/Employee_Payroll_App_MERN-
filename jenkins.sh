92fc934686956d6087248b82030798b25d12261c
Mohit <mohitshah7777@gmail.com>
2021-07-31 01:24:10 +0530

    refactor: jenkins.sh file

--- jenkins.sh
+++ jenkins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
 cp .env /home/ubuntu/Employee_Payroll_BackEnd/
-cd /home/ubuntu/Employee_Payroll_Backend
+cd /home/ubuntu/Employee_Payroll_BackEnd
 directory=$(pwd)
 echo "Directory is $directory"
 pm2 delete 0
 npm install
 npm start
-pm2 --name Employee_Payroll_Frontend start npm -- start
+pm2 --name Employee_Payroll_BackEnd start npm -- start
 # npm i
 # npm run build
 # npm start
